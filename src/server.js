876a11ae50d52b9ac3bb76d9387207e4dc183ef4
akhil-pc <akhilbandari9999@gmail.com>
2021-06-13 20:13:52 +0530

    added cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const connectDb = require('./db')
 
 connectDb()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json({ extended: false }))
 
 app.use('/notes', require('./routes/notes'))
